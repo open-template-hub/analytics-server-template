e07582ad01cbeddca25b78c74b92d5b22f4f564a
furknyavuz <furknyavuz@gmail.com>
2021-02-18 17:54:04 +0300

    Code Format by Rules: https://github.com/open-template-hub/open-template-hub.github.io/blob/master/assets/ide-config/OTHCodeStyle.xml

--- app/data/event.data.ts
+++ app/data/event.data.ts
@@ -19,18 +19,18 @@ export class EventDataModel {
       timestamp: { type: Number, required: true },
       payload: { type: Object },
     };
-    this.dataSchema = new mongoose.Schema(schema);
+    this.dataSchema = new mongoose.Schema( schema );
   }
 
   /**
    * creates provider model
    * @returns provider model
    */
-  getDataModel = async (conn: mongoose.Connection) => {
+  getDataModel = async ( conn: mongoose.Connection ) => {
     return conn.model(
-      this.collectionName,
-      this.dataSchema,
-      this.collectionName
+        this.collectionName,
+        this.dataSchema,
+        this.collectionName
     );
   };
 }
--- app/route/index.route.ts
+++ app/route/index.route.ts
@@ -1,21 +1,15 @@
 import {
-  router as monitorRouter,
-  publicRoutes as monitorPublicRoutes,
-} from './monitor.route';
-import {
-  router as eventRouter,
-  adminRoutes as eventAdminRoutes,
-} from './event.route';
-import { NextFunction, Request, Response } from 'express';
-import {
   context,
-  ErrorHandlerUtil,
+  DebugLogUtil,
   EncryptionUtil,
+  ErrorHandlerUtil,
   MongoDbProvider,
   PreloadUtil,
-  DebugLogUtil,
 } from '@open-template-hub/common';
+import { NextFunction, Request, Response } from 'express';
 import { Environment } from '../../environment';
+import { adminRoutes as eventAdminRoutes, router as eventRouter, } from './event.route';
+import { publicRoutes as monitorPublicRoutes, router as monitorRouter, } from './monitor.route';
 
 const subRoutes = {
   root: '/',
@@ -31,84 +25,84 @@ export module Routes {
   var publicRoutes: string[] = [];
   var adminRoutes: string[] = [];
 
-  function populateRoutes(mainRoute: string, routes: Array<string>) {
+  function populateRoutes( mainRoute: string, routes: Array<string> ) {
     var populated = Array<string>();
-    for (var i = 0; i < routes.length; i++) {
-      const s = routes[i];
-      populated.push(mainRoute + (s === '/' ? '' : s));
+    for ( var i = 0; i < routes.length; i++ ) {
+      const s = routes[ i ];
+      populated.push( mainRoute + ( s === '/' ? '' : s ) );
     }
 
     return populated;
   }
 
-  export const mount = (app: any) => {
+  export const mount = ( app: any ) => {
     environment = new Environment();
-    mongodb_provider = new MongoDbProvider(environment.args());
+    mongodb_provider = new MongoDbProvider( environment.args() );
     const preloadUtil = new PreloadUtil();
 
     preloadUtil
-      .preload(mongodb_provider)
-      .then(() => console.log('DB preload is completed.'));
+    .preload( mongodb_provider )
+    .then( () => console.log( 'DB preload is completed.' ) );
 
-    publicRoutes = [...populateRoutes(subRoutes.monitor, monitorPublicRoutes)];
-    console.log('Public Routes: ', publicRoutes);
+    publicRoutes = [ ...populateRoutes( subRoutes.monitor, monitorPublicRoutes ) ];
+    console.log( 'Public Routes: ', publicRoutes );
 
-    adminRoutes = [...populateRoutes(subRoutes.event, eventAdminRoutes)];
-    console.log('Admin Routes: ', adminRoutes);
+    adminRoutes = [ ...populateRoutes( subRoutes.event, eventAdminRoutes ) ];
+    console.log( 'Admin Routes: ', adminRoutes );
 
     const responseInterceptor = (
-      req: Request,
-      res: Response,
-      next: NextFunction
+        req: Request,
+        res: Response,
+        next: NextFunction
     ) => {
       let originalSend = res.send;
-      const encryptionUtil = new EncryptionUtil(environment.args());
+      const encryptionUtil = new EncryptionUtil( environment.args() );
       res.send = function () {
-        debugLogUtil.log('Starting Encryption: ', new Date());
-        let encrypted_arguments = encryptionUtil.encrypt(arguments);
-        debugLogUtil.log('Encryption Completed: ', new Date());
+        debugLogUtil.log( 'Starting Encryption: ', new Date() );
+        let encrypted_arguments = encryptionUtil.encrypt( arguments );
+        debugLogUtil.log( 'Encryption Completed: ', new Date() );
 
-        originalSend.apply(res, encrypted_arguments as any);
+        originalSend.apply( res, encrypted_arguments as any );
       } as any;
 
       next();
     };
 
     // Use this interceptor before routes
-    app.use(responseInterceptor);
+    app.use( responseInterceptor );
 
     // INFO: Keep this method at top at all times
-    app.all('/*', async (req: Request, res: Response, next: NextFunction) => {
+    app.all( '/*', async ( req: Request, res: Response, next: NextFunction ) => {
       try {
         // create context
         res.locals.ctx = await context(
-          req,
-          environment.args(),
-          publicRoutes,
-          adminRoutes,
-          mongodb_provider
+            req,
+            environment.args(),
+            publicRoutes,
+            adminRoutes,
+            mongodb_provider
         );
 
         next();
-      } catch (err) {
-        let error = errorHandlerUtil.handle(err);
-        res.status(error.code).json({ message: error.message });
+      } catch ( err ) {
+        let error = errorHandlerUtil.handle( err );
+        res.status( error.code ).json( { message: error.message } );
       }
-    });
+    } );
 
     // INFO: Add your routes here
-    app.use(subRoutes.monitor, monitorRouter);
-    app.use(subRoutes.event, eventRouter);
+    app.use( subRoutes.monitor, monitorRouter );
+    app.use( subRoutes.event, eventRouter );
 
     // Use for error handling
-    app.use(function (
-      err: Error,
-      req: Request,
-      res: Response,
-      next: NextFunction
+    app.use( function (
+        err: Error,
+        req: Request,
+        res: Response,
+        next: NextFunction
     ) {
-      let error = errorHandlerUtil.handle(err);
-      res.status(error.code).json({ message: error.message });
-    });
+      let error = errorHandlerUtil.handle( err );
+      res.status( error.code ).json( { message: error.message } );
+    } );
   };
 }
