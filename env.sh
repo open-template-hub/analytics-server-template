9f92f76ccc8bbd99393ae33597e424cc98b6af8c
Furkan Yavuz <furknyavuz@gmail.com>
2021-09-25 11:17:45 +0300

    configs updated

---
+++ env.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [ ! -f .env ]; then
+  echo "Generating .env file.."
+  touch .env
+  {
+    echo "PORT=4005"
+
+    echo "PROJECT=OTH"
+    echo "MODULE=AnalyticsServer"
+    echo "ENVIRONMENT=Local"
+
+    echo "MONGODB_URI={Database Connection Url}"
+    echo "MONGODB_CONNECTION_LIMIT={MongoDB Connection Limit}"
+
+    echo "CLOUDAMQP_APIKEY={MQ Api Key}"
+    echo "CLOUDAMQP_URL={MQ Connection Url}"
+
+    echo "ANALYTICS_SERVER_QUEUE_CHANNEL=oth_analytics_queue"
+    echo "ORCHESTRATION_SERVER_QUEUE_CHANNEL=oth_orchestration_queue"
+
+    echo "ACCESS_TOKEN_SECRET={Access Token Secret}"
+    echo "RESPONSE_ENCRYPTION_SECRET={Response Encryption Secret}"
+
+  } >>.env
+else
+  echo ".env file already exists. Nothing to do..."
+fi
