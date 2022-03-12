5d13da87825df666e1f583bfb325345970274fb7
fatihturker <fatihturker35@gmail.com>
2022-03-12 19:09:00 +0300

    Add Provider Availability

--- app/route/index.route.ts
+++ app/route/index.route.ts
@@ -21,7 +21,14 @@ export namespace Routes {
   export const mount = (app: any) => {
     const envArgs = new Environment().args();
 
-    const ctxArgs = { envArgs } as ContextArgs;
+    const ctxArgs = {
+      envArgs,
+      providerAvailability: {
+        mongo_enabled: true,
+        postgre_enabled: false,
+        mq_enabled: true,
+      },
+    } as ContextArgs;
 
     const assets = {
       mqChannelTag: envArgs.mqArgs
