a08f576d8fad513a981883fa7f618e686aa22dac
Fatih Turker <fatihturker35@gmail.com>
2023-01-27 10:02:08 +0300

    Update environment

--- environment.ts
+++ environment.ts
@@ -1,7 +1,7 @@
 import { DbArgs, EnvArgs, MqArgs, TokenArgs } from '@open-template-hub/common';
 
 export class Environment {
-  constructor( private _args: EnvArgs = {} as EnvArgs ) {
+  constructor(private _args: EnvArgs = {} as EnvArgs) {
     const tokenArgs = {
       accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
       responseEncryptionSecret: process.env.RESPONSE_ENCRYPTION_SECRET,
@@ -10,18 +10,20 @@ export class Environment {
     const dbArgs = {
       mongoDbConnectionLimit: process.env.MONGODB_CONNECTION_LIMIT,
       mongoDbUri: process.env.MONGODB_URI,
+      redisUri: process.env.REDISCLOUD_URL,
+      redisConnectionLimit: process.env.REDIS_CONNECTION_LIMIT,
     } as DbArgs;
 
     const mqArgs = {
       messageQueueConnectionUrl: process.env.CLOUDAMQP_URL,
       analyticsServerMessageQueueChannel:
-      process.env.ANALYTICS_SERVER_QUEUE_CHANNEL,
+        process.env.ANALYTICS_SERVER_QUEUE_CHANNEL,
       orchestrationServerMessageQueueChannel:
-      process.env.ORCHESTRATION_SERVER_QUEUE_CHANNEL,
+        process.env.ORCHESTRATION_SERVER_QUEUE_CHANNEL,
     } as MqArgs;
 
     const serverSpecificArgs = {
-      maxQueryLimit: process.env.MAX_QUERY_LIMIT
+      maxQueryLimit: process.env.MAX_QUERY_LIMIT,
     } as any;
 
     this._args = { tokenArgs, dbArgs, mqArgs, serverSpecificArgs } as EnvArgs;
